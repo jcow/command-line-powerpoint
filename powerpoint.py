d48d9043cb839a33e33823b0efa6f098d7e42e64
Jason Cowan <jasoncow@zillow.com>
2016-04-09 10:47:21 -0700

    Add some polish

--- powerpoint.py
+++ powerpoint.py
@@ -2,23 +2,30 @@ import os
 import time
 import sys
 
-
-
-
 def read_textual_powerpoint(filepath):
 	blocks = []
 	with open(filepath, "r") as ins:
+		line_number = 0
 		current_block = []
 		for line in ins:
 			new_block = False
 			
-			if len(line) > 0 and line[0] != "" and line[0] != "\t" and line[0] != "\n":
+			if len(line) > 0 and line[0].isalpha() and len(current_block) > 0:
 				blocks.append(current_block)
 				current_block = []
 
+			# print line
 			current_block.append(line)
+			line_number += 1
+
+		if len(current_block) > 0:
+			blocks.append(current_block)
 
 	return blocks
+
+def print_blocks(blocks):
+	for block in blocks:
+		print_block(block)
 
 def print_block(block):
 	for line in block:
@@ -30,17 +37,16 @@ def clear_page():
 
 def app(filepath):
 	blocks = read_textual_powerpoint(filepath)
-	print blocks
 
 	def inp_loop(block_index=0):
 		clear_page()
 		print_block(blocks[block_index])
 
-		inp = raw_input(':')
+		inp = raw_input('\n:')
 
-		if inp == 'j':
+		if inp == 'j' or inp == 'd':
 			block_index -= 1
-		if inp == 'k':
+		if inp == 'k' or inp == 'f':
 			block_index += 1
 		if inp == 'q':
 			sys.exit()
